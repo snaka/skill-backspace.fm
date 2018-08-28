a7d769609667a10071366065d3f33aa137f4a39d
snaka <snaka.gml@gmail.com>
2018-08-29 00:00:47 +0900

    ローカライズ用resource更新

--- lambda/custom/resources/ja-JP.js
+++ lambda/custom/resources/ja-JP.js
@@ -1,9 +1,12 @@
 module.exports = {
   translation: {
     SPEECH_START_PLAYING_EPISODE: '%s の最新エピソード「%s」を再生します',
-    SPEECH_INVALID_EPISODE_INDEX: 'ごめんなさい、最近の%dエピソードまでしか対応していません。何番目のエピソードが聴きたいですか？',
+    SPEECH_INVALID_EPISODE_INDEX: 'エピソードの番号は1から%sまでの数字で指定してください。何番目のエピソードが聴きたいですか？',
     SPEECH_START_PLAYING_EPISODE_AT: '%s の %d 番目のエピソード「%s」を再生します',
     SPEECH_START_OVER: '先頭から再生します',
+    SPEECH_INVALID_FAST_FORWARD_MINUTES: '早送りする分数を指定してください。何分進めますか？',
+    SPEECH_INVALID_REWIND_MINUTES: '巻き戻しする分数を指定してください。何分戻しますか？',
+    SPEECH_PLAYER_STATE_IS_NOT_PLAYING: 'その操作はエピソードを再生中の場合のみ可能です',
     SPEECH_FASTFORWARD_X_MIN: '%d分進めます',
     SPEECH_REWIND_X_MIN: '%d分戻ります',
     SPEECH_HELP: 'バックスペースエフエムプレイヤーではバックスペースエフエムで配信中の最新から%d番目のエピソードを聴くことができます。何番目のエピソードが聴きたいですか？',
@@ -15,6 +18,8 @@ module.exports = {
     SPEECH_ERROR_OCCURRED: 'ごめんなさい、よく理解できませんでした',
     CARD_TITLE_START_PLAYING: '%s の最新エピソード',
     PROMPT_INDEX_NUMBER: '何番目のエピソードが聴きたいですか？',
+    PROMPT_FAST_FORWARD_MINUTES: '何分進めますか？',
+    PROMPT_REWIND_MINUTES: '何分戻しますか？',
     CARD_TITLE_INVALID_EPISODE: '対応していないエピソード',
     CARD_TITLE_PLAYING_EPISODE_AT: '%s の %d 番目のエピソード',
     CARD_TITLE_ABOUT_SKILL: 'backspace.fm プレイヤーについて',
