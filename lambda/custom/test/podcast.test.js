decf6cb4e27796fdeaa8ef24389c57d3d7957ae8
snaka <snaka.gml@gmail.com>
2018-09-28 06:57:22 +0900

    saveToCacheのテスト

--- lambda/custom/test/podcast.test.js
+++ lambda/custom/test/podcast.test.js
@@ -6,19 +6,18 @@ const nock = require('nock')
 const expect = chai.expect
 const podcast = rewire('../podcast.js')
 
+const stubConfig = {
+  FEED_URL: 'http://feed.example.com/podcast',
+  TABLE_NAME: 'dynamodb-test',
+  ID: 'podcast-id',
+  NAME: 'podcast name',
+  NAME_LOCALIZED: 'ポッドキャスト',
+  MAX_EPISODE_COUNT: 10
+}
+podcast.__set__('targetPodcast', stubConfig)
+podcast.__set__('exports.config', stubConfig)
+
 beforeEach(() => {
-  const stubConfig = {
-    FEED_URL: 'http://feed.example.com/podcast',
-    TABLE_NAME: 'dynamodb-test',
-    ID: 'podcast-id',
-    NAME: 'podcast name',
-    NAME_LOCALIZED: 'ポッドキャスト',
-    MAX_EPISODE_COUNT: 10
-  }
-  podcast.__set__('targetPodcast', stubConfig)
-  podcast.__set__('exports.config', stubConfig)
-
-
   // XRay関連のメソッドがエラーになるのでstub化する
   podcast.__set__('awsXRay', {
     captureAWS(obj) {
@@ -69,7 +68,6 @@ describe('podcast module', () => {
     const fetchHead = podcast.__get__('fetchHead')
 
     context('リモートから正常のレスポンスが返ってくる場合', () => {
-      // RSSフィードの読み込みをMockに差し替える
       nock('http://feed.example.com')
         .head('/podcast')
         .reply(200, '', {
@@ -84,13 +82,59 @@ describe('podcast module', () => {
     })
 
     context('リモートからエラーが返ってくる場合', () => {
-      // RSSフィードの読み込みをMockに差し替える
       nock('http://feed.example.com')
         .head('/podcast')
         .reply(404, '', { })
+
       it('エラーを返す', async () => {
         const head = await fetchHead('http://feed.example.com/podcast')
         expect(head.statusCode).to.equal(404)
+      })
+    })
+  })
+
+  describe('saveToCache', () => {
+    const mockDynamoDB = {
+      _tableName: '',
+      _item: {},
+      put(obj) {
+        this._tableName = obj.TableName
+        this._item = obj.Item
+        return this
+      },
+      promise() {
+        return Promise.resolve()
+      }
+    }
+    const saveToCache = podcast.__get__('saveToCache')
+
+    beforeEach(() => {
+      mockDynamoDB._tableName = ''
+      mockDynamoDB._item = {}
+      podcast.__set__('getDynamoDB', () => mockDynamoDB)
+    })
+
+    it('DynamoDB に episodes と headers がキャッシュされる', async () => {
+      await saveToCache('podcastA', [{
+          published_at: '2018-09-28T00:00:00.000Z',
+          title: 'Episode Title #001',
+          url: 'https://example.com/episode-001.mp3'
+        }], {
+          etag: 'abcdefg1234'
+        }
+      )
+
+      expect(mockDynamoDB._tableName).to.equal(stubConfig.TABLE_NAME)
+      expect(mockDynamoDB._item).to.deep.include({
+        podcastId: 'podcastA',
+        episodes: [{
+            published_at: '2018-09-28T00:00:00.000Z',
+            title: 'Episode Title #001',
+            url: 'https://example.com/episode-001.mp3'
+        }],
+        headers: {
+          etag: 'abcdefg1234'
+        }
       })
     })
   })
