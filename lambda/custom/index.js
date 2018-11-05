0ac4bd8a08564f8b96de0aa5367612c916b453bb
snaka <snaka.gml@gmail.com>
2018-11-06 00:21:10 +0900

    PodcastSkillBuilderクラス追加

--- lambda/custom/index.js
+++ lambda/custom/index.js
@@ -2,50 +2,14 @@
 /* eslint-disable  no-console */
 'use strict'
 
-const Alexa = require('ask-sdk-core')
 const Adapter = require('ask-sdk-dynamodb-persistence-adapter')
-const interceptors = require('./interceptors')
-const handlers = require('./handlers')
-const PodcastPlayer = require('./podcast-player')
+const PodcastSkillBuilder = require('./podcast-skill-builder')
+const podcastConfig = require('./constants')
 
-PodcastPlayer.podcastConfig = require('./constants')
-for (let name in handlers) {
-  handlers[name].PodcastPlayer = PodcastPlayer
-}
-
-const DynamoDBAdapter = new Adapter.DynamoDbPersistenceAdapter({
+const skillBuilder = new PodcastSkillBuilder(podcastConfig)
+const dynamoDBAdapter = new Adapter.DynamoDbPersistenceAdapter({
   tableName: process.env.PERSISTENT_STORE_TABLE,
   createTable: true
 })
-const skillBuilder = Alexa.SkillBuilders.custom()
 
-exports.handler = skillBuilder
-  .withPersistenceAdapter(DynamoDBAdapter)
-  .addRequestInterceptors(
-    interceptors.localizationInterceptor,
-    interceptors.requestLoggingInterceptor,
-    interceptors.defineUtilityInterceptor
-  )
-  .addResponseInterceptors(
-    interceptors.responseLoggingInterceptor,
-    interceptors.savePersistentAttributesInterceptor
-  )
-  .addRequestHandlers(
-    handlers.warmUpHandler,
-    handlers.playPodcastIntentHandler,
-    handlers.playPodcastByIndexIntentHandler,
-    handlers.startOverIntentHandler,
-    handlers.fastForwardIntentHandler,
-    handlers.rewindIntentHandler,
-    handlers.helpIntentHandler,
-    handlers.cancelAndStopIntentHandler,
-    handlers.resumeIntentHandler,
-    handlers.nextIntentHandler,
-    handlers.previousIntentHandler,
-    handlers.unsupportedIntentHandler,
-    handlers.audioPlayerEventHandler,
-    handlers.sessionEndedRequestHandler,
-    handlers.systemExceptionHandler
-  )
-  .addErrorHandlers(handlers.errorHandler)
-  .lambda()
+exports.handler = skillBuilder.build(dynamoDBAdapter)
