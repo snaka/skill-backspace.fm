8ebf9228c946503f6f1a1ad9c23321d277064c32
snaka <snaka.gml@gmail.com>
2018-11-09 00:34:23 +0900

    config変数名の変更に追随

--- lambda/custom/constants.js
+++ lambda/custom/constants.js
@@ -1,10 +1,10 @@
 module.exports = {
-  SKILL_NAME: 'backspace.fm プレイヤー',
-  SKILL_NAME_LOCALIZED: 'バックスペースエフエムプレイヤー',
-  FEED_URL: 'http://feeds.backspace.fm/backspacefm',
-  TABLE_NAME: 'alexa-skill-podcasts-player',
-  ID: 'backspace.fm',
-  NAME: 'backspace.fm',
-  NAME_LOCALIZED: 'バックスペースエフエム',
-  MAX_EPISODE_COUNT: 500
+  skillName: 'backspace.fm プレイヤー',
+  skillNameForSpeech: 'バックスペースエフエムプレイヤー',
+  feedUrl: 'http://feeds.backspace.fm/backspacefm',
+  tableName: 'alexa-skill-podcasts-player',
+  podcastId: 'backspace.fm',
+  podcastName: 'backspace.fm',
+  podcastNameForSpeech: 'バックスペースエフエム',
+  maxEpisodeCount: 500
 }
